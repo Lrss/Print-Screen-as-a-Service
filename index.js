3f73dd7392c6020a8e4409d6ec68636411eb1125
Lars Sørensen <dev@lrss.dk>
2024-08-31 16:26:38 +0200

    Added Express.js api endpoint

--- index.js
+++ index.js
@@ -1,67 +1,80 @@
+const express = require('express')
 const puppeteer = require("puppeteer");
 
-async function takeScreenshot(
-  url,
-  selector = null,
-  pageWidth = null,
-  pageHeight = null,
-  screenshotWidth = null,
-  screenshotHeight = null,
-  screenshotPositionX = 0,
-  screenshotPositionY = 0,
-) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--disable-setuid-sandbox"],
-    ignoreHTTPSErrors: true,
-  });
-  const page = await browser.newPage();
+app = express();
 
-  if (pageWidth !== null || pageHeight !== null) {
-    if (pageWidth === null || pageHeight === null) {
-      throw new Error("Both pageWidth and pageHeight must be specified");
+app.get("/", async (request, response) => {
+  const {
+    url,
+    pageWidth,
+    pageHeight,
+    selector,
+    filetype = "png",
+    screenshotWidth,
+    screenshotHeight,
+    screenshotPositionX = 0,
+    screenshotPositionY = 0
+  } = request.query;
+  try {
+    if (!url) {
+      throw new Error("You need to specify a 'url' to capture.");
     }
-    await page.setViewport({ width: pageWidth, height: pageHeight });
+    console.log(`Info: Launching webbrowser (${url})`);
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--disable-setuid-sandbox"],
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    if (pageWidth || pageHeight) {
+      if (!pageWidth || !pageHeight) {
+        throw new Error("Both pageWidth and pageHeight must be specified");
+      }
+      await page.setViewport({ width: parseInt(pageWidth), height: parseInt(pageHeight) });
+    }
+    console.log(`Info: Loading webpage (${url})`);
+    await page.goto(url, { waitUntil: "networkidle2" });
+    var imageBuffer;
+    if (selector) {
+      if (screenshotWidth || screenshotHeight){
+        throw new Error("screenshotWidth and screenshotHeight have no effect when specifying a selector element");
+      }
+      console.log(`Info: Taking element screenshot (${url})`);
+      await page.waitForSelector(selector);
+      const element = await page.$(selector);
+      imageBuffer = await element.screenshot({type: filetype});
+    } else if (screenshotWidth || screenshotHeight) {
+      if (!screenshotWidth || !screenshotHeight) {
+        throw new Error("Both screenshotWidth and screenshotHeight must be specified");
+      }
+      console.log(`Info: Taking snippet screenshot (${url})`);
+      imageBuffer = await page.screenshot({
+        clip: {
+          x: parseInt(screenshotPositionX),
+          y: parseInt(screenshotPositionY),
+          width: parseInt(screenshotWidth),
+          height: parseInt(screenshotHeight),
+        },
+        type: filetype
+      });
+    } else {
+      console.log(`Info: Taking full page screenshot (${url})`);
+      imageBuffer = await page.screenshot({
+        fullPage: true,
+        type: filetype
+      });
+    }
+    await browser.close();
+    response.set('content-type', 'image/' + filetype);
+    response.write(imageBuffer,'binary')
+    response.end(null, 'binary')
+  } catch (error) {
+    console.log(error);
+    response.status(400);
+    response.send({ "status": 400,"message": error.message })
   }
-  await page.goto(url, { waitUntil: "networkidle2" });
-  if (selector !== null) {
-    if (screenshotWidth !== null || screenshotHeight !== null){
-      throw new Error("screenshotWidth and screenshotHeight have no effect when specifying a selector element");
-    }
-    await page.waitForSelector(selector);
-    const element = await page.$(selector);
-    await element.screenshot({
-      type: "png",
-      path: "screenshot2.png",
-    });
-  } else if (screenshotWidth !== null || screenshotHeight !== null) {
-    if (screenshotWidth === null || screenshotHeight === null) {
-      throw new Error("Both screenshotWidth and screenshotHeight must be specified");
-    }
-    await page.screenshot({
-      clip: {
-        x: screenshotPositionX,
-        y: screenshotPositionY,
-        width: screenshotWidth,
-        height: screenshotHeight,
-      },
-      type: "png",
-      path: "screenshot.png",
-    });
-  } else {
-    await page.screenshot({
-      fullPage: true,
-      type: "png",
-      path: "screenshot.png",
-    });
-  }
+});
 
-  await browser.close();
-}
-
-takeScreenshot(
-  url = "https://plotly.com/javascript/responsive-fluid-layout/",
-  selector = ".main-svg",
-  pageWidth = 1920,
-  pageHeigth = 1080,
-);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port http://localhost:' + listener.address().port);
+});
